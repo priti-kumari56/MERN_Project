c65bbe8e25cdcc7be23fbfef6e4b3ef4a530fb7a
Priti <pritikumari200565@gmail.com>
2025-06-18 09:11:43 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="container text-center">
+      <h1>Welcome to MERN Projects</h1>
+    </div>
+  );
+}
+
+export default App;
