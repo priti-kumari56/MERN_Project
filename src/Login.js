3b9c27efb579c3336adb6a8a9082f300edff1480
Priti <pritikumari200565@gmail.com>
2025-06-19 09:12:11 +0530

    Push

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,88 @@
 import React from 'react';
+import { useState } from 'react';
+
+function Login() {
+    const [formData, setFormData]=useState({
+        username:"",
+        password:""
+    });
+
+    const [errors,setErrors]=useState({});
+    const [message,setMessage] =useState(null);
+
+    const handleChange= (event) =>{
+        const name=event.target.name;
+        const value=event.target.value;
+
+        console.log(name);
+        console.log(value);
+
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const validate =() =>{
+        let newErrors= {};
+        let isValid = true;
+        if(formData.username.length === 0){
+            newErrors.username = "Username is mandatory";
+            isValid= false;
+    
+        }
+        if(formData.password.length === 0){
+            newErrors.password ="Password is mandatory";
+            isValid =false;
+        }
+        setErrors(newErrors);
+        return isValid;
+    }
 
 
-function App() {
+    const handleSubmit =(event) =>{
+        event.preventDefault();
+
+        if(validate()){
+        if(formData.username === 'admin' && formData.password==='admin'){
+            setMessage('Correct Credentials');
+        }
+        else{
+            setMessage('Invalid Credentials');
+        }
+    }
+    };
+
+
   return (
     <div className="container text-center">
       <h1>Welcome to Login Page</h1>
+      {message && (
+        message
+      )}
+      <form onSubmit={handleSubmit}>
+        <div>
+            <label>Username:</label>
+            <input type="text" name="username" 
+            value={formData.username}
+            onChange={handleChange}
+            />
+            {errors.username && (errors.username)}
+        </div>
+        <div>
+            <label>Password:</label>
+            <input type="password" name="password" 
+            value={formData.password}
+            onChange={handleChange}
+            />
+            {errors.password && (errors.password)}
+        </div>
+        <div>
+            <button>Login</button>
+        </div>
+      </form>
     </div>
   );
 }
 
-export default App;
+export default Login;
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,11 +1,35 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 function Header(){
     return(
 
-        <div className="container">
-            <Link to='/'>Home</Link>
-            <Link to="/login">Login</Link>
-        </div>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <Link  className="navbar-brand" to="/">
+      Home
+    </Link>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <Link className="nav-link" to="/login">
+          Login
+        </Link>
+       
+        
+      </div>
+    </div>
+  </div>
+</nav>
+
     );
 }
 
